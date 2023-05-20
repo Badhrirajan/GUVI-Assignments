bea7f616d465b367db975a0e049868fea1ac5540
BADHRIRAJAN T <badhrirajan2211@gmail.com>
2023-05-20 16:39:47 +0530

    Day5

--- Day3 TASK/compare.js
+++
@@ -1,8 +0,0 @@
-let obj1 = {age:5,name:"Person1"};
-let obj2 = {name:"Person1",age:5};
-
-if(obj1.age == obj2.age && obj1.name == obj2.name){
-    console.log("Equal");
-} else {
-    console.log("NotEqual");
-}
--- Day3 TASK/flag.js
+++
@@ -1,9 +0,0 @@
-function flagdata() {
-    const countries = JSON.parse(this.responseText);
-    countries.forEach(co => console.log(co.flags));
-}
-
-var request = new XMLHttpRequest();
-request.addEventListener("load", flagdata);
-request.open("GET", "https://restcountries.com/v3.1/all");
-request.send();
